3702da19e381d500fe29baef9cc2f33960b9ad21
Ritiksingh895 <ritik691singh@gmail.com>
2024-02-24 18:19:29 +0530

    first commit

---
+++ Map.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+public class Map {
+    public static void main(String[] args) {
+        HashMap <String,Integer>hm=new HashMap();
+        hm.put("india",1000);
+        hm.put("aus",100);
+        hm.put("pak",10);
+        hm.put("srilanaka",1);
+        System.out.println(hm);
+    }
+}
